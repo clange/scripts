0bcf21e1c8d78814da19ecb3432d3a48cb88eed8
Christoph Lange <math.semantic.web@gmail.com>
2015-08-31 11:46:35 +0200

    installation

--- compress-screenshots/compress-screenshots.ps1
+++ compress-screenshots/compress-screenshots.ps1
@@ -7,6 +7,25 @@
 
 # Source: http://superuser.com/questions/226828/how-to-monitor-a-folder-and-trigger-a-command-line-action-when-a-file-is-created
 
+param ( [switch] $Install = $false )
+
+# install in startup folder
+if ($Install) {
+    # obtain full path of this script
+    $path = $script:MyInvocation.MyCommand.Path
+    $basename = $(Split-Path $path -Leaf)
+    # obtain startup folder
+    $startup = [Environment]::GetFolderPath('Startup')
+    # create shell link to script in startup folder
+    $WshShell = New-Object -comObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut("$startup\$basename.lnk")
+    $Shortcut.TargetPath = 'powershell'
+    $Shortcut.Arguments = "-NoProfile -ExecutionPolicy Bypass -NoExit -NoLogo -WindowStyle Hidden -File `"$path`""
+    $Shortcut.WindowStyle = 7 # Minimized
+    $Shortcut.Save()
+    exit
+}
+
 $screenshotDir = "$env:USERPROFILE\Pictures\Screenshots"
 Set-Location $screenshotDir
 
