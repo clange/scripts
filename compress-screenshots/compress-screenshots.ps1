a19fe37b1e88bceb92b0c4e6c0036c3700ec6e27
Christoph Lange <math.semantic.web@gmail.com>
2015-11-07 21:55:47 +0100

    factored out installation to new library script

--- compress-screenshots/compress-screenshots.ps1
+++ compress-screenshots/compress-screenshots.ps1
@@ -10,18 +10,8 @@ param ( [switch] $Install = $false )
 
 # install in startup folder
 if ($Install) {
-    # obtain full path of this script
-    $path = $script:MyInvocation.MyCommand.Path
-    $basename = $(Split-Path $path -Leaf)
-    # obtain startup folder
-    $startup = [Environment]::GetFolderPath('Startup')
-    # create shell link to script in startup folder
-    $WshShell = New-Object -comObject WScript.Shell
-    $Shortcut = $WshShell.CreateShortcut("$startup\$basename.lnk")
-    $Shortcut.TargetPath = 'powershell'
-    $Shortcut.Arguments = "-NoProfile -ExecutionPolicy Bypass -NoExit -NoLogo -WindowStyle Hidden -File `"$path`""
-    $Shortcut.WindowStyle = 7 # Minimized
-    $Shortcut.Save()
+    . '..\lib\install-in-startup.ps1'
+    installInStartup
     exit
 }
 
