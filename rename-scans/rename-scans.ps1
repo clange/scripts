42908d191acab6151f986798168fec979e1d8f28
Christoph Lange <math.semantic.web@gmail.com>
2017-05-13 23:21:01 +0200

    fixed behaviour in subdirectories of the directory watched

--- rename-scans/rename-scans.ps1
+++ rename-scans/rename-scans.ps1
@@ -31,7 +31,12 @@ $watcher = New-Object IO.FileSystemWatcher -Property $watcherProps
 # if (*) not present, rename to (1)
 # else if (1)…(9) present, rename to (n+1)
 $renameAction = {
-    $name = $Event.SourceEventArgs.Name
+    # Perform all actions in the directory in which the affected file has been created.
+    $directory = (Get-Item $Event.SourceEventArgs.FullPath).Directory.FullName
+    Set-Location $directory
+    # Obtain the name of the affected file.  Just its name, not including the path relative to the directory watched.
+    $name = $Event.SourceEventArgs.Name.Split('\')[-1]
+    # [System.Windows.Forms.MessageBox]::Show("Handling the creation of $name in $pwd", 'rename-scans')
     $maxIndex = 0
     # get all existing scan files with number suffixes
     $existing = Get-ChildItem 'Scan_???????? (*).png' | Where-Object {$_.Name -Match 'Scan_........ \([0-9]+\)\.png$'}
@@ -46,11 +51,13 @@ $renameAction = {
         }
     }
     $maxIndex++
-    Rename-Item $name ($name -Replace '\.png', " ($maxIndex).png")
+    $newName = $name -Replace '\.png$', " ($maxIndex).png"
+    Rename-Item $name $newName
+    # [System.Windows.Forms.MessageBox]::Show("Renamed $name to $newName", 'rename-scans')
     # if we have arrived at the next power of 10, …
     if ($maxIndex -Match '^10+$') {
         # … add leading zeroes to all previous names so that lexicographic sorting has the same effect as numeric sorting
-        # [System.Windows.Forms.MessageBox]::Show("\$maxIndex = $maxIndex", 'Debug')
+        # [System.Windows.Forms.MessageBox]::Show("\$maxIndex = $maxIndex", 'rename-scans')
         foreach ($file in $existing) {
             Rename-Item $file ($file -Replace '\(([0-9]+\)\.png)', '(0$1')
         }
