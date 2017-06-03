bd987396bc56e2f96fa3b050ccc1ac0161bade07
Christoph Lange <math.semantic.web@gmail.com>
2017-06-03 06:06:31 +0200

    also including wakeup from hibernate; displaying 10 events

--- last-resume/last-resume.ps1
+++ last-resume/last-resume.ps1
@@ -1,4 +1,4 @@
-# last-resume.ps1: output the time the system was last resumed from sleep
+# last-resume.ps1: output the times the system was most recently resumed from sleep
 
 # © Christoph Lange <math.semantic.web@gmail.com> 2017–
 
@@ -6,6 +6,8 @@
 
 Get-WinEvent -LogName System `
   | Where { `
-    $_.ProviderName -eq 'Microsoft-Windows-Kernel-Power' `
-    -and $_.Message -eq 'The system has resumed from sleep.' } | `
-  Select -First 1 -Property TimeCreated
+      ($_.ProviderName -eq 'Microsoft-Windows-Kernel-Power' `
+      -and $_.Message -eq 'The system has resumed from sleep.') `
+    -or ($_.ProviderName -eq 'Microsoft-Windows-Power-Troubleshooter' `
+      -and $_.Message -Match '^The system has returned from a low power state') } | `
+  Select -First 10 -ExpandProperty TimeCreated
