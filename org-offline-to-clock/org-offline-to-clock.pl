8b4a21444d8fe3bbc993a912a5ef967e574f1405
Christoph Lange <math.semantic.web@gmail.com>
2017-07-18 20:28:26 +0200

    fixed some date conversion bugs

--- org-offline-to-clock/org-offline-to-clock.pl
+++ org-offline-to-clock/org-offline-to-clock.pl
@@ -11,13 +11,13 @@ use v5.10;
 use List::MoreUtils qw(firstidx);
 use POSIX qw(mktime strftime);
 
-my ($year, $weekday, $weekday_strftime, $day, $month, $from_str, $to_str, $from, $to, $diff, $diff_minutes, @clocks);
+my ($year, $weekday, $weekday_strftime, $day, $month, @timestamp, $from_str, $to_str, $from, $to, $diff, $diff_minutes, @clocks);
 my @MONTHS = qw(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec);
 
 while (<>) {
     if (/^([0-9]{4})$/) {
         # start of a "year" block
-        $year = $1;
+        $year = $1 - 1900;
         print;
     } elsif (/^(Mon|Tue|Wed|Thu|Fri|Sat|Sun) ([0-9]{1,2}) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)/) {
         # start of a "day" block
@@ -25,13 +25,14 @@ while (<>) {
         $weekday = $1;
         $day = $2;
         $month = (firstidx { $_ eq $3 } @MONTHS);
+        @timestamp = localtime(mktime(0, 0, 0, $day, $month, $year));
         # check week day for consistency
-        $weekday_strftime = strftime('%a', 0, 0, 0, $day, $month, $year - 1900);
+        $weekday_strftime = strftime('%a', @timestamp);
         if ($1 ne $weekday_strftime) {
-            printf STDERR "$day $3: Noted week day (%s) is different from actual week day of date (%s); using the latter.\n", $1, $weekday_strftime;
+            printf STDERR "$2 $3: Noted week day (%s) is different from actual week day of date (%s); using the latter.\n", $1, $weekday_strftime;
         }
         # output heading for this day
-        printf("* [%s]\n", strftime('%Y-%m-%d %a', 0, 0, 0, $day, $month, $year - 1900));
+        printf("* [%s]\n", strftime('%Y-%m-%d %a', @timestamp));
     } elsif (/^(.*?) ((?:[0-9]{1,2}:[0-9]{2}-[0-9]{1,2}:[0-9]{2}, ?)+)/) {
         # an event with clock logs
         # open LOGBOOK drawer
@@ -41,14 +42,16 @@ while (<>) {
         foreach (reverse @clocks) {
             # for each interval (most recent first)
             if (/([0-9]{1,2}):([0-9]{2})-([0-9]{1,2}):([0-9]{2})/) {
-                $from = mktime(0, $2, $1, $day, $month, $year - 1900);
-                $to = mktime(0, $4, $3, $day, $month, $year - 1900);
-                $diff = $to - $from;
+                $from = mktime(0, $2, $1, $day, $month, $year);
+                $to = mktime(0, $4, $3, $day, $month, $year);
                 # if end is less than start then we assume it's on the next day
-                if ($diff < 0) { $diff += 60 * 60 * 24; }
+                if ($to < $from) {
+                    $to += 60 * 60 * 24;
+                }
+                $diff = $to - $from;
                 $diff_minutes = $diff / 60;
-                $from_str = strftime('%Y-%m-%d %a %H:%M', 0, $2, $1, $day, $month, $year - 1900);
-                $to_str = strftime('%Y-%m-%d %a %H:%M', 0, $4, $3, $day, $month, $year - 1900);
+                $from_str = strftime('%Y-%m-%d %a %H:%M', localtime($from));
+                $to_str = strftime('%Y-%m-%d %a %H:%M', localtime($to));
                 printf("   CLOCK: [%s]--[%s] => %2d:%02d\n", $from_str, $to_str, $diff_minutes / 60, $diff_minutes % 60);
             }
         }
