8be9e21f2bb5f3c60bd3edebc1280fad35160037
Christoph Lange <math.semantic.web@gmail.com>
2019-07-22 11:48:19 +0200

    show notification

--- compress-screenshots/compress-screenshots.ps1
+++ compress-screenshots/compress-screenshots.ps1
@@ -1,6 +1,6 @@
 # compress-screenshots.ps1: runs optipng (http://optipng.sourceforge.net) to compress all screenshots upon creation
 
-# © Christoph Lange <math.semantic.web@gmail.com> 2015–
+# © Christoph Lange <math.semantic.web@gmail.com> 2015–2019
 
 # Download and licensing information available at https://github.com/clange/scripts
 
@@ -31,7 +31,20 @@ $watcher = New-Object IO.FileSystemWatcher -Property $watcherProps
 # set up the event handler
 $compressAction = {
     $name = $Event.SourceEventArgs.Name
+    $sizeBefore = (Get-Item $name).Length / 1024
+    # compress the image
     & optipng -o5 $name
+    $sizeAfter = (Get-Item $name).Length / 1024
+    # display notification
+    Add-Type -AssemblyName System.Windows.Forms 
+    $global:balloon = New-Object System.Windows.Forms.NotifyIcon
+    $shellPath = (Get-Process -id $pid).Path
+    $balloon.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($shellPath)
+    $balloon.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Info 
+    $balloon.BalloonTipText = "Screenshot {0} reduced from {1:N2} KiB to {2:N2} KiB" -f $name, $sizeBefore, $sizeAfter
+    $balloon.BalloonTipTitle = 'Compressed Screenshot'
+    $balloon.Visible = $true 
+    $balloon.ShowBalloonTip(5000)
 }
 
 # register the event handler
