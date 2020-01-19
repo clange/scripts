47115e411754c5bee27476779b2d914347cf4c51
Christoph Lange <math.semantic.web@gmail.com>
2020-01-19 17:55:16 +0100

    changing defaults to one single partition

--- cyg-rsnap/cyg-rsnap-c.bat
+++ cyg-rsnap/cyg-rsnap-c.bat
@@ -4,7 +4,11 @@ rem from the path where cyg-rsnap-c.bat is located,
 rem e.g. like this:
 rem bash -c 'cd path/to/cyg-rsnap; vscsc -exec="cyg-rsnap-c.bat" c:'
 rem It maps a VSS of c: to x:
-rem and chains into a script that maps a VSS of d: to y:
+rem and (disabled) chains into a script that maps a VSS of d: to y:
+echo %1
+vscsc -q
 dosdev x: %1
-vscsc -exec="cyg-rsnap-d.bat" d:
+dir x:
+rem vscsc -exec="cyg-rsnap-d.bat" d:
+c:\tools\cygwin\bin\bash.exe -x ./cyg-rsnap.sh
 dosdev /d x:
--- cyg-rsnap/cyg-rsnap.sh
+++ cyg-rsnap/cyg-rsnap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 # /etc/rsnapshot.conf assumes the following drive letters to exist:
 # x: (/cygdrive/x): a VSS of c:
-# y: (/cygdrive/y): a VSS of d:
+# (currently disabled) y: (/cygdrive/y): a VSS of d:
 # rsnapshot -V daily
 rsnapshot -V sync && \
     rsnapshot -V daily
